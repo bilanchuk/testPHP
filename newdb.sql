204337987006ebd8603ad33169a3c13bd1f271df
bilanchuk <39571005+bilanchuk@users.noreply.github.com>
2019-03-17 11:43:18 +0200

    Add files via upload

---
+++ newdb.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Час створення: Бер 17 2019 р., 10:40
+-- Версія сервера: 10.1.38-MariaDB
+-- Версія PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База даних: `newdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `dishes`
+--
+-- Помилка читання структури таблиці newdb.dishes: #1932 - Table 'newdb.dishes' doesn't exist in engine
+-- Помилка читання даних таблиці newdb.dishes: #1064 - У вас помилка у синтаксисі SQL біля 'FROM `newdb`.`dishes`' в строці 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
